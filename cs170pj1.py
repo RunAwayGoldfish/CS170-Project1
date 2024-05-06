54ff2fe625eb02a8fa483cb27e66af7cd85c63b8
NateBeatty <natebeatty10@protonmail.com>
2024-05-05 23:43:03 -0700

    Add all files to GitHub

--- cs170pj1.py
+++ cs170pj1.py
@@ -1,18 +1,27 @@
 import random
 import numpy as np
 import copy
+from operator import attrgetter
+
 
 class SlidingPuzzle:
-    def __init__(self, size=3, hash=None):
+    def __init__(self, size=3, hash=None, mode=0):
         self.size = size
         self.board = self.generateBoard()
         self.parent = None
-        self.weight = 0
+        self.weight = 0 # f(n)
+        self.distance = 0 # Steps from origin aka g(n)
+        self.hueristic = 0 
+        self.mode = mode
+
         
         if(hash == None):
             self.board = self.generateBoard()
         else:
             self.board = (np.array(hash).reshape((int(size), -1))).tolist()
+
+
+
 
     def generateBoard(self):
         numbers = list(range(1, self.size ** 2))
@@ -37,7 +46,6 @@ class SlidingPuzzle:
         else:
             return False
         return True
-        
 
     def findBlank(self):
         for i in range(self.size):
@@ -65,8 +73,11 @@ class SlidingPuzzle:
             validMove = newPuzzle.move(i)
             if(validMove):
                 newPuzzle.parent = self
-                newPuzzle.weight = self.weight+1
+                newPuzzle.distance = self.distance+1
+                newPuzzle.applyHueristic()
+                newPuzzle.applyWeight()
                 variantList.append(newPuzzle)
+
         return variantList
 
     def duplicate(self):
@@ -84,10 +95,25 @@ class SlidingPuzzle:
         self.printBoard()
         while(parent != None):
             print("====================")
+            print(parent.weight, parent.distance, parent.hueristic)
             parent.printBoard()
             parent = parent.parent
-            
 
+    def applyHueristic(self):
+        score = 0
+        if(self.mode == 1):
+            score = 0
+            # misplaced tile hueristic
+            for k in range(self.size * self.size - 1):
+                i = int(k / self.size)
+                j = k % self.size
+                if(self.board[i][j] != str(i*self.size + j + 1)):
+                    score +=1
+
+        self.hueristic = score
+
+    def applyWeight(self):
+        self.weight = self.distance + self.hueristic
 
 # Example usage:
 #puzzle = SlidingPuzzle()
@@ -95,12 +121,13 @@ class SlidingPuzzle:
 size = 3
 #puzzle = SlidingPuzzle(size, [1,2,3,4,5,6,7,"*",8])
 #puzzle = SlidingPuzzle(size, [1,2,3,4,5,6,"*",7,8])
-puzzle = SlidingPuzzle(size)
+puzzle = SlidingPuzzle(size, mode=1)
 puzzle.printBoard()
 frontier = []
 visited = []
 frontier.append(puzzle)
 foundSolution = False
+
 while(len(frontier) != 0):
     currentPuzzle = frontier[0]
     visited.append(currentPuzzle.createID())
@@ -118,9 +145,14 @@ while(len(frontier) != 0):
         if(i.createID() not in visited):
             frontier.append(i)
 
+    frontier = sorted(frontier, key=lambda x: x.weight)
+
     frontier = frontier[1:]
     if(len(frontier)%100 == 0):
         print(len(frontier))
+
+
+
 
 
 print(foundSolution)
