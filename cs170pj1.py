33f40e364f026783d7ffe81cf7bba0ab168b5569
NateBeatty <natebeatty10@protonmail.com>
2024-05-06 00:20:36 -0700

    Updating code

--- cs170pj1.py
+++ cs170pj1.py
@@ -1,11 +1,10 @@
 import random
 import numpy as np
 import copy
-from operator import attrgetter
 
 
 class SlidingPuzzle:
-    def __init__(self, size=3, hash=None, mode=0):
+    def __init__(self, size=3, setup=None, mode=0):
         self.size = size
         self.board = self.generateBoard()
         self.parent = None
@@ -15,13 +14,10 @@ class SlidingPuzzle:
         self.mode = mode
 
         
-        if(hash == None):
+        if(setup == None):
             self.board = self.generateBoard()
         else:
-            self.board = (np.array(hash).reshape((int(size), -1))).tolist()
-
-
-
+            self.board = (np.array(setup).reshape((int(size), -1))).tolist()
 
     def generateBoard(self):
         numbers = list(range(1, self.size ** 2))
@@ -101,14 +97,21 @@ class SlidingPuzzle:
 
     def applyHueristic(self):
         score = 0
-        if(self.mode == 1):
+        if(self.mode == 1): # misplaced tile hueristic
             score = 0
-            # misplaced tile hueristic
             for k in range(self.size * self.size - 1):
                 i = int(k / self.size)
                 j = k % self.size
                 if(self.board[i][j] != str(i*self.size + j + 1)):
                     score +=1
+        if(self.mode == 2): #Euclidian? Using manhattan because it seems like that's what it's meant to be:
+            for i in range(self.size):
+                for j in range(self.size):
+                    if self.board[i][j] == '*':
+                        continue
+                    goal_i = int(int(self.board[i][j] - 1) / self.size)
+                    goal_j = (int(self.board[i][j] - 1)) % self.size
+                    score += goal_i - i + goal_j - j
 
         self.hueristic = score
 
@@ -121,53 +124,72 @@ class SlidingPuzzle:
 size = 3
 #puzzle = SlidingPuzzle(size, [1,2,3,4,5,6,7,"*",8])
 #puzzle = SlidingPuzzle(size, [1,2,3,4,5,6,"*",7,8])
-puzzle = SlidingPuzzle(size, mode=1)
-puzzle.printBoard()
-frontier = []
-visited = []
-frontier.append(puzzle)
-foundSolution = False
-
-while(len(frontier) != 0):
-    currentPuzzle = frontier[0]
-    visited.append(currentPuzzle.createID())
-
-    #currentPuzzle.printBoard()
-    #print("=============================")
-
-    if(currentPuzzle.isSolved()):
-        print(currentPuzzle.weight)
-        currentPuzzle.printChain()
-        foundSolution = True
-        break
-
-    for i in currentPuzzle.createVariants():
-        if(i.createID() not in visited):
-            frontier.append(i)
-
-    frontier = sorted(frontier, key=lambda x: x.weight)
-
-    frontier = frontier[1:]
-    if(len(frontier)%100 == 0):
-        print(len(frontier))
 
 
+#puzzle = SlidingPuzzle(size, setup=[1,2,"*",4,5,3,7,8,6],mode=0)
 
 
+# "Oh Boy"
+#puzzle = SlidingPuzzle(size, setup=[8,7,1,6,"*",2,5,4,3],mode=0)    # True 
+#puzzle = SlidingPuzzle(size, setup=[8,7,1,6,"*",2,5,4,3],mode=1)   # True | 10720 visited
 
-print(foundSolution)
-'''
-while(not puzzle.isSolved() and False):
-    puzzle = SlidingPuzzle(size)
-puzzle.printBoard()
-print(puzzle.createID())
-print("==========")
-#puzzle.createVariants()
-'''
-'''
-while not puzzle.isSolved():
-    direction = input("Enter direction (up/down/left/right): ")
-    puzzle.move(direction)
-    puzzle.print_board()
-print("Congratulations! You solved the puzzle!")
-'''
+#puzzle = SlidingPuzzle(size, setup=[1,2,3,4,5,6,8,7,"*"],mode=0) 
+
+cases = [
+    [1,2,3,4,5,6,7,8,"*"], # Trivial
+    [1,2,"*",4,5,3,7,8,6], # 
+    [8,7,1,6,"*",2,5,4,3],
+    [1,2,3,4,5,6,7,"*",8],
+    ["*",1,2,4,5,3,7,8,6],
+    [1,2,3,4,5,6,8,7,"*"] # Impossible
+]
+
+def getData():
+    for i in cases:
+        arr = i
+        for j in range(3):
+            print(arr)
+            puzzle2 = SlidingPuzzle(3, arr, j)
+            solvePuzzle(puzzle2)
+
+
+def solvePuzzle(puzzle, mode=0):
+    frontier = []
+    visited = []
+    frontier.append(puzzle)
+    foundSolution = False
+
+    nodesChecked = 0 # Note that this is unique nodes becuase I do not add duplicates to frontier
+
+    while(len(frontier) != 0):
+        currentPuzzle = frontier[0]
+        visited.append(currentPuzzle.createID())
+        nodesChecked +=1
+
+        #currentPuzzle.printBoard()
+        #print("=============================")
+
+        if(currentPuzzle.isSolved()):
+            if(mode >= 1):
+                currentPuzzle.printChain()
+            foundSolution = True
+            break
+
+        for i in currentPuzzle.createVariants():
+            if(i.createID() not in visited):
+                frontier.append(i)
+                
+
+        frontier = sorted(frontier, key=lambda x: x.weight) # Should be already sorted if not using a hueristic?
+
+        frontier = frontier[1:]
+        if(mode == 2):
+            if(len(frontier)%100 == 0):
+                print(len(frontier))
+
+    print(foundSolution, nodesChecked)
+
+
+#puzzle = SlidingPuzzle(size=3,setup=[1,2,3,4,5,6,7,8,"*"],mode=2)
+#solvePuzzle(puzzle,1)
+getData()
