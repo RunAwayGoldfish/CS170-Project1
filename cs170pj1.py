41ccc8410c7b59b2e0c51e2a8545d4e9c13b09ae
NateBeatty <natebeatty10@protonmail.com>
2024-05-06 23:43:06 -0700

    Completed

--- cs170pj1.py
+++ cs170pj1.py
@@ -1,9 +1,12 @@
 import random
 import numpy as np
 import copy
-
+import time
+import math
+import bisect
 
 class SlidingPuzzle:
+    maxDist = 0
     def __init__(self, size=3, setup=None, mode=0):
         self.size = size
         self.board = self.generateBoard()
@@ -12,35 +15,46 @@ class SlidingPuzzle:
         self.distance = 0 # Steps from origin aka g(n)
         self.hueristic = 0 
         self.mode = mode
-
+        self.step = ""
+        self.blank_i = -1
+        self.blank_j = -1
         
         if(setup == None):
             self.board = self.generateBoard()
         else:
             self.board = (np.array(setup).reshape((int(size), -1))).tolist()
 
+        self.blank_i, self.blank_j = self.findBlank()
+
     def generateBoard(self):
-        numbers = list(range(1, self.size ** 2))
+        numbers = list(range(1, (self.size ** 2)))
         numbers = [str(x) for x in numbers] + ["*"]
         random.shuffle(numbers)
         return [numbers[i:i+self.size] for i in range(0, len(numbers), self.size)]
 
     def printBoard(self):
+        padding = int(math.log10(self.size * self.size)+1)
         for row in self.board:
-            print(" ".join(str(cell).rjust(2) for cell in row))
+            print(" ".join(str(cell).rjust(padding) for cell in row))
 
     def move(self, direction):
-        row, col = self.findBlank()
+        row, col = self.blank_i, self.blank_j
+
         if direction == 'up' and row > 0:
             self.board[row][col], self.board[row-1][col] = self.board[row-1][col], self.board[row][col]
+            self.blank_i -= 1
         elif direction == 'down' and row < self.size - 1:
             self.board[row][col], self.board[row+1][col] = self.board[row+1][col], self.board[row][col]
+            self.blank_i += 1
         elif direction == 'left' and col > 0:
             self.board[row][col], self.board[row][col-1] = self.board[row][col-1], self.board[row][col]
+            self.blank_j -= 1
         elif direction == 'right' and col < self.size - 1:
             self.board[row][col], self.board[row][col+1] = self.board[row][col+1], self.board[row][col]
+            self.blank_j += 1
         else:
             return False
+        self.step = direction
         return True
 
     def findBlank(self):
@@ -74,6 +88,10 @@ class SlidingPuzzle:
                 newPuzzle.applyWeight()
                 variantList.append(newPuzzle)
 
+                if(newPuzzle.distance > self.__class__.maxDist):
+                    self.__class__.maxDist = newPuzzle.distance
+
+
         return variantList
 
     def duplicate(self):
@@ -87,13 +105,42 @@ class SlidingPuzzle:
         return ID
 
     def printChain(self):
-        parent = self.parent
+        expansion = []
+        moveSolution = []
+        
+
+        position = self
+        #self.printBoard()
+        while(position != None):
+            #print("====================")
+            #print(parent.weight, parent.distance, parent.hueristic)
+            expansion.insert(0, position)
+            moveSolution.insert(0, position.move)
+
+            position = position.parent
+
+        print("Optimal solution is as follows:")
+        for i in range(len(expansion)):
+            if(i == 0):
+                print("Starting Position")
+                expansion[i].printBoard()
+                continue
+            print("The optimal move is next optimal move is '{0}': which yields the state below:".format(expansion[i].step))
+            expansion[i].printBoard()
+
+        print("Solved!")
+            
+        
+    def printBoardv2(self):
+        if(self.distance == 0):
+            print("Initial State: ")
+        else:
+            print("The best expansion is: g(n) = {0} and h(n) = {1}".format(self.distance, self.hueristic))
+
         self.printBoard()
-        while(parent != None):
-            print("====================")
-            print(parent.weight, parent.distance, parent.hueristic)
-            parent.printBoard()
-            parent = parent.parent
+        print()
+
+
 
     def applyHueristic(self):
         score = 0
@@ -104,19 +151,26 @@ class SlidingPuzzle:
                 j = k % self.size
                 if(self.board[i][j] != str(i*self.size + j + 1)):
                     score +=1
+
         if(self.mode == 2): #Euclidian? Using manhattan because it seems like that's what it's meant to be:
             for i in range(self.size):
                 for j in range(self.size):
-                    if self.board[i][j] == '*':
+                    if self.board[i][j] == "*":
                         continue
-                    goal_i = int(int(self.board[i][j] - 1) / self.size)
-                    goal_j = (int(self.board[i][j] - 1)) % self.size
-                    score += goal_i - i + goal_j - j
+
+                    goal_i = (int(self.board[i][j]) - 1) // self.size
+                    goal_j = (int(self.board[i][j]) - 1) % self.size
+                    # Basically converts position (value in the cell) between 2d and 1d array
+
+                    score += abs(goal_i - i) + abs(goal_j - j)
 
         self.hueristic = score
 
     def applyWeight(self):
         self.weight = self.distance + self.hueristic
+
+    def resetMaxDist(self):
+        self.__class__.maxDist = 0
 
 # Example usage:
 #puzzle = SlidingPuzzle()
@@ -137,10 +191,10 @@ size = 3
 
 cases = [
     [1,2,3,4,5,6,7,8,"*"], # Trivial
-    [1,2,"*",4,5,3,7,8,6], # 
-    [8,7,1,6,"*",2,5,4,3],
+    [1,2,"*",4,5,3,7,8,6], #
     [1,2,3,4,5,6,7,"*",8],
     ["*",1,2,4,5,3,7,8,6],
+    [8,7,1,6,"*",2,5,4,3],
     [1,2,3,4,5,6,8,7,"*"] # Impossible
 ]
 
@@ -150,46 +204,99 @@ def getData():
         for j in range(3):
             print(arr)
             puzzle2 = SlidingPuzzle(3, arr, j)
-            solvePuzzle(puzzle2)
+            solvePuzzle(puzzle2,mode=0)
+            puzzle2.resetMaxDist()
+
+def bisectionSort(arr, element):
+    bisect.insort(arr, element, key=lambda x: -x.weight)
 
 
 def solvePuzzle(puzzle, mode=0):
     frontier = []
-    visited = []
+    visited = set()
     frontier.append(puzzle)
     foundSolution = False
-
+    startTime = time.time()
     nodesChecked = 0 # Note that this is unique nodes becuase I do not add duplicates to frontier
+    maxDist = 0
+    SolDepth = 0
+    queueSize = 1
+    maxQueueSize = 1
+    currentPuzzle = None
 
     while(len(frontier) != 0):
-        currentPuzzle = frontier[0]
-        visited.append(currentPuzzle.createID())
+        currentPuzzle = frontier.pop()
+        visited.add(currentPuzzle.createID())
         nodesChecked +=1
+        currentPuzzle.printBoardv2()
 
-        #currentPuzzle.printBoard()
-        #print("=============================")
-
+        
         if(currentPuzzle.isSolved()):
-            if(mode >= 1):
-                currentPuzzle.printChain()
+            SolDepth = currentPuzzle.distance
+            print("Solved!")
+            print()
             foundSolution = True
             break
 
         for i in currentPuzzle.createVariants():
-            if(i.createID() not in visited):
-                frontier.append(i)
-                
+            if(not (i.createID() in visited)):
+                bisectionSort(frontier,i)
+                queueSize+=1
 
-        frontier = sorted(frontier, key=lambda x: x.weight) # Should be already sorted if not using a hueristic?
+        if(queueSize > maxQueueSize):
+            maxQueueSize = queueSize        
 
-        frontier = frontier[1:]
+        #frontier = sorted(frontier, key=lambda x: x.weight) # Should be already sorted if not using a hueristic
+
+        queueSize-=1
+
         if(mode == 2):
             if(len(frontier)%100 == 0):
-                print(len(frontier))
+                print(len(frontier), currentPuzzle.maxDist)
+       
+    if(foundSolution):
+        currentPuzzle.printChain()
+    else:
+        print("No solution")
+        
+    if(mode > 1):
+        endTime = time.time()
+        print(foundSolution, nodesChecked, endTime - startTime, currentPuzzle.maxDist, SolDepth, queueSize)
 
-    print(foundSolution, nodesChecked)
+    
 
 
-#puzzle = SlidingPuzzle(size=3,setup=[1,2,3,4,5,6,7,8,"*"],mode=2)
-#solvePuzzle(puzzle,1)
-getData()
+#puzzle = SlidingPuzzle(size=3,setup=[8,7,1,6,"*",2,5,4,3],mode=2)
+#solvePuzzle(puzzle,2)
+#getData()
+
+def main():
+    print("Enter integers separated by spaces with 0 to indicate the space: ")
+    print("for example entering: \"1 2 3 4 5 6 7 8 0\" creates:")
+    testPuzzle = SlidingPuzzle(3, setup=[1,2,3,4,5,6,7,"*",8],mode=0)
+    testPuzzle.printBoard()
+    user_input = input("Enter your values here: ")
+    
+
+    
+    arr = user_input.split()
+    arrSize = len(arr)
+    
+    for x in range(len(arr)):
+        if arr[x] == "0":
+            arr[x] = "*"
+    
+    print(arr)
+    
+    print("Enter the number for the corresponding algorithm you wish to run:")
+    print("1 - Uniform Cost Search")
+    print("2 - A* with the Misplaced Tile heuristic")
+    print("3 - A* with the Euclidean distance heuristic")
+    user_input = int(input()) - 1
+    
+
+    Puzzle = SlidingPuzzle(size=int(len(arr) ** 0.5), setup=arr, mode = user_input)
+    solvePuzzle(Puzzle, mode=0)
+
+
+main()
